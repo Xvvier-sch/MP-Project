397ede8888e8fe425073754ca35b4f87354c6e1c
Xvvier-sch <2000771b@student.tp.edu.sg>
2023-01-03 12:48:57 +0800

    test

--- Encryption/function.php
+++ Encryption/function.php
@@ -22,7 +22,7 @@ function caesarCipher($str, $amount) {
 	$output1 = [];
 
 	if ($amount < 0) {
-		return caesarShift($str, $amount + 26);
+		return caesarCipher($str, $amount + 26);
 	}
 	
     //Caesar Cipher - Shift Forwards
@@ -37,13 +37,10 @@ function caesarCipher($str, $amount) {
 			}
 		}
         $output[]=$c;
-		
 	}
-	//print_r($output);
     $forward=implode('', $output);
 	unset($output);
 	$output1["foward $amount"]=$forward ;
-	//print_r($output1);
 
     //Caesar Cipher - Shift Backwards
     for ($i = 0; $i < strlen($str); $i++) {
@@ -59,11 +56,37 @@ function caesarCipher($str, $amount) {
         $output[]=$c;
 	}
     $backwards=implode('', $output);
-	//echo $backwards;
-	$output1["Backward $amount"]=$backwards ;
-	//print_r($output1);
+	$output1["Backward $amount"]=$backwards;
 	$a=implode(' ', $output1);
 	echo $a;
 }
+
+//Caesar Cipher Bruteforce 
+function caesarDecryptBF($str){
+    global $forward;
+    global $backwards;
+    $output = [];
+    $output1 = [];
+    for ($amount = 1; $amount < 26; $amount++) {
+        //Caesar Cipher - Shift Forwards
+        for ($i = 0; $i < strlen($str); $i++) {
+            $c = $str[$i];
+            if (preg_match("/[a-z]/i", $c)) {
+                $code = ord($str[$i]);
+                if ($code >= 65 && $code <= 90) {
+                    $c = chr((($code - 65 + $amount) % 26) + 65);
+                } elseif ($code >= 97 && $code <= 122) {
+                    $c = chr((($code - 97 + $amount) % 26) + 97);
+                }
+            }
+            $output[]=$c;
+        }
+        $forward=implode('', $output);
+        unset($output);
+        $output1["foward $amount"]=$forward ;
+    }
+    print_r($output1);
+}
+
 
 ?>
